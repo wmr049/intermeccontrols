c511c051dbbc50e592bc99e8da12e16030f9ee09
hjgode <hjgode@d736ad0b-6eb7-15bd-295a-cf9b3c672e84>
2011-08-26 11:04:04 +0000

    older release (V4)

---
+++ DPAG4/Hasci.TestApp.And_Controls/HasciTestApp/MenuForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Hasci.TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.labelPerCent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(0, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(480, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Barcode";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(0, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(480, 76);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Foto mit Kamera";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.button3.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(0, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(480, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Foto mit Scanner";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.button4.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(0, 344);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(480, 76);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Adresseingabe über Bildschirmtastatur";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.button5.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(0, 420);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(480, 76);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Adresseingabe über Gerätetastatur";
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.button6.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(0, 496);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(480, 76);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Unterschrift";
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // labelPerCent
+            // 
+            this.labelPerCent.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelPerCent.Location = new System.Drawing.Point(271, 42);
+            this.labelPerCent.Name = "labelPerCent";
+            this.labelPerCent.Size = new System.Drawing.Size(195, 46);
+            this.labelPerCent.Text = "label1";
+            this.labelPerCent.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(480, 574);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelPerCent);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Hauptmenü";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Closing += new System.ComponentModel.CancelEventHandler(this.Form1_Closing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label labelPerCent;
+    }
+}
+
